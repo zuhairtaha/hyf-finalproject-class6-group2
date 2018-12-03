ab808adb9dd6ad446cc744cf67c873fcae8a803e
Zuhair Taha <admin@tahasoft.com>
2018-12-03 22:15:29 +0100

    change mentors.js to js module

--- src/server/server.js
+++ src/server/server.js
@@ -1,18 +1,16 @@
 const express = require("express")
 const path = require("path")
 const bodyParser = require("body-parser")
-
+const mentors = require("./mentors")
 const app = express()
 const port = process.env.PORT || 4000
-const fs = require('fs')
-
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 // API calls
 app.get('/api/mentors', (req, res) => {
-  fs.readFile(path.join(__dirname, './mentors.json'), 'utf8', (err, data) => res.send(data))
+  res.send(mentors)
 })
 
 
