85129ec8879914e2ec0581feb49c3b6f20d83c8d
Zuhair Taha <admin@tahasoft.com>
2018-12-03 21:57:43 +0100

    remove unneccssery post

--- src/server/server.js
+++ src/server/server.js
@@ -1,36 +1,29 @@
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
+const express = require("express")
+const path = require("path")
+const bodyParser = require("body-parser")
 
-const app = express();
-const port = process.env.PORT || 4000;
-const fs = require('fs');
+const app = express()
+const port = process.env.PORT || 4000
+const fs = require('fs')
 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 // API calls
 app.get("/api/mentors", (req, res) => {
-  const mentors = fs.readFileSync(path.join(__dirname,'mentors.json'), 'utf8');
-  res.send(mentors);
-});
-
-app.post("/api/world", (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`
-  );
-});
+  const mentors = fs.readFileSync(path.join(__dirname, 'mentors.json'), 'utf8')
+  res.send(mentors)
+})
 
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, "../../build")));
+  app.use(express.static(path.join(__dirname, "../../build")))
 
   // Handle React routing, return all requests to React app
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../../build", "index.html"));
-  });
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../../build", "index.html"))
+  })
 }
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}`))
