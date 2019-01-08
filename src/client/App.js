69b9391eed332a4b1e3056cddc0bbc0559d06bfd
Zuhair Taha <tahasoft2@gmail.com>
2019-01-08 08:48:52 +0100

    adding Joe auth repo

--- src/client/App.js
+++ src/client/App.js
@@ -3,13 +3,14 @@ import Navbar from './components/layouts/Navbar'
 //import Jumbotron from './components/layouts/Jumbotron'
 import Main from './components'
 // import Footer from './components/layouts/Footer'
-
+import Auth from './components/auth/containers/App'
 class App extends Component {
 
   render = () =>
     <>
       <Navbar />
-      <Main />
+      <Auth />
+      {/*<Main />*/}
       {/*<Footer />*/}
     </>
 }
--- src/server/app.js
+++ src/server/app.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const authRouter = require('./auth/routes')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
 const clientRouter = require('./routes/client')
 const apiRoutes = require('./routes')
+// Even though we don't use this variable anywhere, if it's not required the authentication dosen't work.
+// I'll get back to it and figure it out.
+// eslint-disable-next-line no-unused-vars
+const passportSetup = require('./auth/passport-setup')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
 
 const app = express()
 
@@ -14,7 +21,6 @@ app.use(express.urlencoded({extended: false}))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use("/api", apiRoutes);
 
 // error handler
 app.use((err, req, res, next) => {
@@ -25,6 +31,36 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500)
   res.json(err.message)
 })
+
+app.use(
+  cookieSession({
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    keys: [process.env.COOKIE_KEY]
+  })
+)
+
+// Then we initialize passport and start the sessions.
+app.use(passport.initialize())
+app.use(passport.session())
+
+/*
+ * Here we make sure that every route that comes from the router.js file start with /auth,
+ * to make it easialy distinguishable from other routes.
+ */
+app.use('/auth', authRouter.router)
+
+/*
+ * Here we check the cookie if the cookie is valid with the middleware we wrote,
+ * them we send the information back if it's vaild and we send nothing back if it's not.
+ * we could send back json with information that the request was invalid if we wanted to
+ * do anything in react to display this.
+ */
+app.post('/api/profile', authRouter.authCheck, (req, res) => {
+  res.send(req.user)
+})
+
+app.use("/api", apiRoutes);
+
 
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
