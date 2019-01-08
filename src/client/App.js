b835c27b7da3ea45013ccf08febc3b4306dcf1aa
Zuhair Taha <tahasoft2@gmail.com>
2019-01-08 09:08:27 +0100

    commit authApp

--- src/client/App.js
+++ src/client/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Navbar from './components/layouts/Navbar'
 //import Jumbotron from './components/layouts/Jumbotron'
-import Main from './components'
+// import Main from './components'
 // import Footer from './components/layouts/Footer'
 import AuthApp from './components/auth/containers/AuthApp'
 class App extends Component {
@@ -9,7 +9,7 @@ class App extends Component {
   render = () =>
     <>
       <Navbar />
-      <AuthApp />
+      {/*<AuthApp />*/}
       {/*<Main />*/}
       {/*<Footer />*/}
     </>
